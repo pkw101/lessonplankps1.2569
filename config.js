077156e6e83cfef2327655261e99608137bc92a4
pkw101 <phakawat.owat@gmail.com>
2026-09-21 17:27:36 +0700

    Update KPS_API_URL to new deployment link

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ตั้งค่าลิงก์ระบบหลังบ้าน (Apps Script Web app)
 // วางลิงก์ที่ได้จากการ Deploy (ลงท้ายด้วย /exec) ระหว่างเครื่องหมายคำพูด
-window.KPS_API_URL = "https://script.google.com/macros/s/AKfycbyFmNhPq1q8kPBPn4XYxcmPllSOCTglyxtIwWcuNaOoc087Hif0z16xeUGlKlEpsp-ubA/exec";
+window.KPS_API_URL = "https://script.google.com/macros/s/AKfycbzl427CxjPzQ2bwdONHAgEK9pBWw_jpk8ixgui8yAF5ldR76rSMOLKJHO1bIqdjkjFtqg/exec";
